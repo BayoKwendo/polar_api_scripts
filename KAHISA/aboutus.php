6e0c33058c23fe4b70668b9a1719aa55e7a69df5
BayoKwendo <bayokwendo@gmail.com>
2021-09-05 00:50:10 +0000

    new website

---
+++ KAHISA/aboutus.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="assets/images/favicon.ico">
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+  <title>KAHISA</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+
+</head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="preloader">
+    <div class="jumper">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>  
+  <!-- ***** Preloader End ***** -->
+
+  <!-- Header -->
+  <header class="">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.html"><h2>KA<em>HISA</em></h2></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li> 
+            <li class="nav-item active"><a class="nav-link" href="aboutus.php">About Us</a></li>
+
+            <li class="nav-item"><a class="nav-link" href="packages.html">Packages</a></li>
+
+            <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+
+            <li class="nav-item dropdown ">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">More</a>
+
+              <div class="dropdown-menu">
+                <a class="dropdown-item active" href="about-us.html">About Us</a>
+                <a class="dropdown-item" href="testimonials.html">Testimonials</a>
+                <a class="dropdown-item" href="terms.html">Terms</a>
+              </div>
+            </li>
+
+            <li class="nav-item"><a class="nav-link" href="contact.html">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Page Content -->
+  <div class="page-heading about-heading header-text" style="background-image: url(assets/images/heading-1-1920x500.jpg);">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-content">
+            <h4>about us</h4>
+            <h2>KAHISA</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="best-features about-features">
+    <div class="container">
+      <div class="col-md-12">
+        <div class="section-heading">
+          <h2>Background Information</h2>
+        </div>
+      </div>
+      <div class="left-content">
+        <p style="text-align: justify;background: transparent;margin-bottom: -60px;line-height: 200%;">Was formed from <b>AHISA</b>. The members namely: 
+         Dr. Edith Apondi, Dr. Irene Inwani, Dr. Juddy Wachira saw the need of having a Kenyan version of AHISA, and so, <b>KAHISA</b> was born. Later other members namely; Paula Braitstein, Kawango Agot, Cyrus Mugo,   Kinuthia John Bernard Baridi, Elizabeth Bukusi, Dr. Mburugu , Dr. Kristin Beima-Sofie, Dr Charlotte Pahe, Ayuku David, Florence Jaguga, Nyandiko Mokaya, Abong&rsquo;o Christopher, Muganda Rosemary, Irene Njuguna joined</p>
+       </div>
+     </div>
+   </div>
+
+
+   <div class="team-members">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Overall Goal of KAHISA</h2>
+          </div>
+          <p>To support the formulation and implementation of strategies that have impact in halting the HIV pandemic among Adolescents and Young Adults (AYA) and improving outcome among AYA living with HIV in Kenya</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="team-members">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Main objective</h2>
+          </div>
+          <p>Capacity building support: To support online training of 3 members in implementation science during the financial year 2021-2022.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="inner-content">
+            <p>Copyright © 2021 KAHISA</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+  <!-- Additional Scripts -->
+  <script src="assets/js/custom.js"></script>
+  <script src="assets/js/owl.js"></script>
+</body>
+
+</html>
